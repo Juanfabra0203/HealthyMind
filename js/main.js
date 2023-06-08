e294d121e6253d6447d5690c62d24217af70ae91
Juanfabra0203 <juanfabra113@gmail.com>
2023-06-08 08:55:30 -0500

    Se agrega el proyecto

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var input = document.getElementById("email");
+var inp = document.getElementById("password");
+
+input.addEventListener("input", function() {
+    
+    input.style.fontFamily = "russ";
+    input.style.padding="10px"
+    input.style.boxSizing="border-box"
+    input.style.color="white";
+s
+});
+
+inp.addEventListener("input", function() {
+    inp.style.fontWeight="700";
+    input.style.fontFamily = "russ";
+    inp.style.padding="10px";
+    inp.style.boxSizing="border-box";
+    inp.style.color="white";
+
+});
+
+
